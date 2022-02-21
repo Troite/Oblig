092386d0cb31fcd899004fbdffe542e371862d27
Elias Trøite <70884282+Troite@users.noreply.github.com>
2022-02-21 20:54:04 +0100

    Update

---
+++ O2/Capitals.py
@@ -0,0 +1,15 @@
+a_dictionary = {}
+a_file = open("USCapitals.txt")
+
+with open("USCapitals.txt") as a_file:
+    for line in a_file:
+        key, value = line.split(", ")
+        a_dictionary[key] = value
+        a_dictionary[key] = value.strip()
+
+state = input("Enter state to search for: ")
+
+if state in a_dictionary:
+    print(a_dictionary[state])
+else:
+    print("Error, invalid state")
---
+++ O2/Fil.py
@@ -0,0 +1,5 @@
+with open("1.txt") as file:
+    words1 = "TODO: get all words of file 1"
+
+with open("2.txt") as file:
+    words2 = "TODO: get all words of file 2"
